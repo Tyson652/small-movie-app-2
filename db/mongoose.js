9874920d12aaed3414df99ce060bc018a92bac7c
Tyson Hofstetter <tysonh652@gmail.com>
2019-06-07 17:03:09 +1000

    eight clone commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/small-movie-app', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/small-movie-app-2', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
